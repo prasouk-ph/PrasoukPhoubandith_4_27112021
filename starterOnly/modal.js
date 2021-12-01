035981c0ae2476aecf19654902a68b27b2da635f
prasouk-ph <prasouk.ph@gmail.com>
2021-12-01 14:03:10 +0100

    Form validation in progress

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,14 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCloseBtn = document.querySelector(".close");
+const modalBtnSubmit = document.querySelector(".btn-submit");
+let firstName = document.querySelector("#first");
+let lastName = document.querySelector("#last");
+let email = document.querySelector("#email");
+let birthdate = document.querySelector("#birthdate");
+let quantity = document.querySelector("#quantity");
+let checkbox = document.querySelector("#checkbox1");
+let text = document.querySelectorAll(".text-control");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -27,4 +35,21 @@ modalCloseBtn.addEventListener("click", closeModal);
 // close modal form
 function closeModal() {
     modalbg.style.display = "none";
-  }
+}
+
+
+// form fulfillement conditions
+modalBtnSubmit.addEventListener("click", checking);
+modalBtnSubmit.style.color = "white";
+modalBtnSubmit.style.background = "gray";
+
+function checking(e) {
+    if (firstName.value == "" || lastName.value == "" || email.value == "" || birthdate.value == "" || checkbox.checked == false) {
+        alert("Saisie invalide")
+        e.preventDefault();
+    } else {
+        alert("gogogogo");
+        modalBtnSubmit.style.background = "red";
+        
+    }
+}
